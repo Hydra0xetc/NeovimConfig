11bd99a74cd850fd7c27dc3515037d4967d15f36
Hydra0xetc <lordxzaodeo@gmail.com>
2025-11-17 13:10:00 +0700

    move set_custom_highlights to options.lua

--- lua/leaf/core/options.lua
+++ lua/leaf/core/options.lua
@@ -57,3 +57,18 @@ vim.opt.wildmode = "full"
 
 -- disable ~
 vim.opt.fillchars = { eob = " " }
+
+-- orange relative number
+local function set_custom_highlights()
+	local colors = {
+		orange = "#ff9e64",
+	}
+	vim.api.nvim_set_hl(0, "LineNr", { fg = colors.orange })
+end
+
+set_custom_highlights()
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+	pattern = "*",
+	callback = set_custom_highlights,
+})
--- lua/leaf/plugins/colorscheme.lua
+++ lua/leaf/plugins/colorscheme.lua
@@ -3,28 +3,13 @@ return {
 	lazy = false,
 	event = { "UIEnter" },
 	priority = 1000,
-	opts = {
-		style = "moon", -- moon, storm, night, day
-		transparent = false,
-		terminal_colors = true,
-	},
-	config = function(_, opts)
-		require("tokyonight").setup(opts)
+	config = function()
+		require("tokyonight").setup({
+			style = "moon", -- moon, storm, night, day
+			transparent = false,
+			terminal_colors = true,
 
-		vim.cmd.colorscheme("tokyonight")
-
-		local function set_custom_highlights()
-			local colors = {
-				orange = "#ff9e64",
-			}
-			vim.api.nvim_set_hl(0, "LineNr", { fg = colors.orange })
-		end
-
-		set_custom_highlights()
-
-		vim.api.nvim_create_autocmd("ColorScheme", {
-			pattern = "*",
-			callback = set_custom_highlights,
+			vim.cmd.colorscheme("tokyonight"),
 		})
 	end,
 }
