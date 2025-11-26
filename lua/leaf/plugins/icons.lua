a04260060b922a02a337b08f876f76838d161005
Hydra0xetc <lordxzaodeo@gmail.com>
2025-11-26 19:10:46 +0700

    update: now lsp have icons

--- lua/leaf/plugins/icons.lua
+++ lua/leaf/plugins/icons.lua
@@ -1,39 +1,81 @@
 return {
-	"nvim-tree/nvim-web-devicons",
-	enabled = true,
-	priority = 1000,
-	opts = {
-		override_by_extension = {
-			["c"] = {
-				color = "#6495ed",
-				icon = "",
-				name = "C",
+	{
+		"nvim-tree/nvim-web-devicons",
+		enabled = true,
+		priority = 1000,
+		opts = {
+			override_by_extension = {
+				["c"] = {
+					color = "#6495ed",
+					icon = "",
+					name = "C",
+				},
+				-- ["python"] = {
+				-- 	color = "#ffff00",
+				-- 	icon = "",
+				-- 	name = "Python",
+				-- },
+				["js"] = {
+					color = "#f7df1e",
+					icon = "",
+					name = "Js",
+				},
+				["html"] = {
+					color = "#ffa07a",
+					icon = "",
+					name = "Html",
+				},
+				["css"] = {
+					color = "#1572b6",
+					icon = "",
+					name = "Css",
+				},
+				-- ["lua"] = {
+				-- 	color = "#366B8A",
+				-- 	icon = "",
+				-- 	name = "Lua",
+				-- },
 			},
-			-- ["python"] = {
-			-- 	color = "#ffff00",
-			-- 	icon = "",
-			-- 	name = "Python",
-			-- },
-			["js"] = {
-				color = "#f7df1e",
-				icon = "",
-				name = "Js",
-			},
-			["html"] = {
-				color = "#ffa07a",
-				icon = "",
-				name = "Html",
-			},
-			["css"] = {
-				color = "#1572b6",
-				icon = "",
-				name = "Css",
-			},
-			-- ["lua"] = {
-			-- 	color = "#366B8A",
-			-- 	icon = "",
-			-- 	name = "Lua",
-			-- },
 		},
+	},
+	{
+		"onsails/lspkind.nvim",
+		config = function()
+			require("lspkind").setup({
+				-- options: 'text', 'text_symbol', 'symbol_text', 'symbol'
+				mode = "symbol_text",
+
+				preset = "codicons",
+
+				-- defaulf symbol
+				symbol_map = {
+					Text = "󰉿",
+					Method = "󰆧",
+					Function = "󰊕",
+					Constructor = "",
+					Field = "󰜢",
+					Variable = "󰀫",
+					Class = "󰠱",
+					Interface = "",
+					Module = "",
+					Property = "󰜢",
+					Unit = "󰑭",
+					Value = "󰎠",
+					Enum = "",
+					Keyword = "󰌋",
+					Snippet = "",
+					Color = "󰏘",
+					File = "󰈙",
+					Reference = "󰈇",
+					Folder = "󰉋",
+					EnumMember = "",
+					Constant = "󰏿",
+					Struct = "󰙅",
+					Event = "",
+					Operator = "󰆕",
+					TypeParameter = "",
+				},
+			})
+		end,
 	},
 }
